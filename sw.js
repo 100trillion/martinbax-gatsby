a29facf7b3fe938f2f6bfdc724b30a808e2428e2
Jean-Luc Leese <jlbleese@gmail.com>
2020-08-11 13:14:42 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-78114076fd7d43942966.js"
+    "url": "webpack-runtime-6f585c07529d0c38b176.js"
   },
   {
     "url": "framework-b5b229d58c3c710d5429.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f5b0b0b06594898b4737bb8e1a7fc07a"
+    "revision": "1b2223b8bccd8d0c36015c9d035fe588"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
